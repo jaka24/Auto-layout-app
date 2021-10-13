697b4faed1b5912b1e1cc9418b0c70423b425741
John-marc Attis <attisjohn@yahoo.com>
2021-10-13 18:37:07 -0400

    Initial Commit

---
+++ Auto layout app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto layout app
+//
+//  Created by John-Marc Attis on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
